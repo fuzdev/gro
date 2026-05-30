4988f485119d6c33cd48f8d2921079b7ec7c4410
Ryan Atkinson <mail@ryanatkn.com>
2026-05-30 01:26:51 -0400

    chore: use 'virtual:fuz.css' instead of the gro generator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import '$routes/fuz.css';
+	import 'virtual:fuz.css';
 	import '@fuzdev/fuz_code/theme.css';
 
 	import ThemeRoot from '@fuzdev/fuz_ui/ThemeRoot.svelte';
 	import type {Snippet} from 'svelte';
 	import {Library, library_context} from '@fuzdev/fuz_ui/library.svelte.js';
 	import {library_json_from_modules} from '@fuzdev/fuz_util/library_json.js';
-	import type {PackageJson} from '@fuzdev/fuz_util/package_json.js';
 	import {modules} from 'virtual:svelte-docinfo';
 
 	import package_json from '../../package.json' with {type: 'json'};
@@ -19,7 +18,7 @@
 		children: Snippet;
 	} = $props();
 
-	const library_json = library_json_from_modules(package_json as PackageJson, modules);
+	const library_json = library_json_from_modules(package_json, modules);
 
 	library_context.set(new Library(library_json));
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite';
 import {sveltekit} from '@sveltejs/kit/vite';
+import {vite_plugin_fuz_css} from '@fuzdev/fuz_css/vite_plugin_fuz_css.js';
 import svelte_docinfo from 'svelte-docinfo/vite.js';
 
 export default defineConfig({
-	plugins: [sveltekit(), svelte_docinfo()],
+	plugins: [vite_plugin_fuz_css(), sveltekit(), svelte_docinfo()],
 	optimizeDeps: {exclude: ['@fuzdev/blake3_wasm']},
 });
