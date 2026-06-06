95d5b92b76171edbcf12341dc1bfd210e0bb702b
Ryan Atkinson <mail@ryanatkn.com>
2026-06-05 22:16:22 -0400

    upgrade dev deps

---
+++ .changeset/upgrade-peer-deps.md
@@ -0,0 +1,5 @@
+---
+'@fuzdev/gro': minor
+---
+
+chore: upgrade peer deps
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import type {Snippet} from 'svelte';
 	import {SiteState, site_context} from '@fuzdev/fuz_ui/site.svelte.js';
 	import {logo_gro} from '@fuzdev/fuz_ui/logos.js';
+	import pkg_json from 'virtual:pkg.json';
 
 	// TODO add website, rewriting the markdown docs as Svelte
 
@@ -15,13 +16,7 @@
 		children: Snippet;
 	} = $props();
 
-	site_context.set(
-		new SiteState({
-			icon: logo_gro,
-			glyph: '🌰',
-			repo_url: 'https://github.com/fuzdev/gro',
-		}),
-	);
+	site_context.set(new SiteState({icon: logo_gro, pkg_json}));
 </script>
 
 <svelte:head>
